030396ffe8fd9828d15f863c02d6f83b4a1d5203
kgyuzalyan <kgyuzalyan@edmunds.com>
2019-02-04 10:59:49 +0300

    TV-Maze-show: Create Routes structure and folders structure

---
+++ src/configs.js
@@ -0,0 +1,4 @@
+export const configs = {
+    apiUrl: 'http://api.tvmaze.com/',
+    SHOW_ID: 6771,
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Routes from './routes';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middlewares = [reduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
